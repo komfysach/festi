954005ecbafe32c5aed839c85d9c44bd38012285
komfysach <sach@getkomfy.net>
2021-02-24 11:08:28 +0200

    broken homepage into small components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Main from './main.component';
-import './main.styles.scss';
+import Main from './pages/main/main.component';
+import './pages/main/main.styles.scss';
 
 function App() {
   return (
---
+++ src/pages/main/main.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './main.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
+
+const Main = () => (
+    <div className='main' >
+        <Directory />
+
+    </div>
+);
+
+export default Main;
